566f933140fc896abd304c26b0451da0352cff8f
alenchufacundo <alenfacurios@gmail.com>
2025-02-04 13:06:35 -0300

    cambios en el widget

--- lib/screens/api/peliculas_populares_screen.dart
+++ lib/screens/api/peliculas_populares_screen.dart
@@ -25,7 +25,10 @@ class _PeliculasPopularesScreenState extends State<PeliculasPopularesScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Películas Populares'),
+        title: Text(
+          'Películas Populares',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
         leading: IconButton(
           icon: Icon(Icons.arrow_back), // Flecha de retroceso
           onPressed: () {
@@ -39,11 +42,44 @@ class _PeliculasPopularesScreenState extends State<PeliculasPopularesScreen> {
               itemCount: movieProvider.popularMovies.length,
               itemBuilder: (context, index) {
                 final movie = movieProvider.popularMovies[index];
-                return ListTile(
-                  leading: Image.network(
-                      'https://image.tmdb.org/t/p/w200${movie.posterPath}'),
-                  title: Text(movie.title),
-                  subtitle: Text(movie.overview),
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.network(
+                          'https://image.tmdb.org/t/p/w200${movie.posterPath}',
+                          width: 120,
+                          height: 180,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(width: 12), // Espacio entre imagen y texto
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              movie.title,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 6),
+                            Text(
+                              movie.overview,
+                              maxLines: 4,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(fontSize: 14, color: Colors.grey[700]),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               },
             ),
